ebfd7b55589c540d140f53658bbb62843313be09
Eugênia Paiva <90074242+eugeniaapaivaa@users.noreply.github.com>
2021-10-03 17:03:16 -0300

    Adicionei os arquivos da AluraFlix

---
+++ script.js
@@ -0,0 +1,9 @@
+var listaAnimes = [
+  "https://d3alv7ekdacjys.cloudfront.net/Custom/Content/Products/11/00/1100139_de-pernas-pro-ar-dvd-filme-comedia-ms_m1_637383540630815589.jpg",
+  "https://s2.glbimg.com/dFyKlsS-CqmFRHg2WxsIR5GkpCs=/362x536/https://s2.glbimg.com/Rjdsdee8bhYyDmITSlIEKQHMaSM=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/0/R/Uw3FeRQImS3hXsxR2Jwg/2020-927-minha-vida-em-marte-poster.jpg",
+  "https://i.pinimg.com/550x/db/76/3c/db763c88aff6f9d276dcac1d7be2adfe.jpg"
+];
+
+for (var i = 0; i < listaAnimes.length; i++) {
+  document.write("<img src=" + listaAnimes[i] + ">");
+}
